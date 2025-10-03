0031c3ad3ff84ccbcc3e94db211d4676c0053883
DuPhat <duphat2003@gmail.com>
2025-10-03 20:16:01 +0700

    first commit

---
+++ src/com/codegym/lession_01/System_time.java
@@ -0,0 +1,11 @@
+package com.codegym.lession_01;
+
+import java.util.Date;
+
+public class System_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
