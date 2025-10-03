aeda0203f0d05873a3c03a5ed60fe08b04da5116
DuPhat <duphat2003@gmail.com>
2025-10-03 20:33:32 +0700

    Add files

---
+++ src/com/codegym/lession_03/Operator_Expression.java
@@ -0,0 +1,18 @@
+package com.codegym.lession_03;
+
+import java.util.Scanner;
+
+public class Operator_Expression {
+    public static void main(String[] args) {
+        float wight;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter wight: ");
+        wight = scanner.nextInt();
+
+        System.out.println("Enter height: ");
+        height = scanner.nextInt();
+    }
+}
